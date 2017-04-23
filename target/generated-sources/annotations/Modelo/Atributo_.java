ba3ae5494c579c4b1d210019dba8f7188c258a3c
Leonardo <leo_lopes09@hotmail.com>
2017-04-23 16:10:49 -0300

    Commit 23/04
    Diversas alterações no código

---
+++ target/generated-sources/annotations/Modelo/Atributo_.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Atributo.class)
+public abstract class Atributo_ {
+
+	public static volatile ListAttribute<Atributo, Componente> componentes;
+	public static volatile SingularAttribute<Atributo, String> valor;
+	public static volatile SingularAttribute<Atributo, Integer> id;
+	public static volatile SingularAttribute<Atributo, TipoAtributo> tipoAtributo;
+
+}
+
