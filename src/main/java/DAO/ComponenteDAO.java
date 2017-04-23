1d87170a960ca3e94c646068670b269a8132350a
Bruno Oseliero de Souza Giampietro <brunooseliero@gmail.com>
2017-04-23 16:25:45 -0300

    Back do ComponenteDAO
    Construindo os métodos de acesso ao banco de dados do componente.

--- src/main/java/DAO/ComponenteDAO.java
+++ src/main/java/DAO/ComponenteDAO.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import Modelo.Componente;
 import Util.JPAUtil;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 
 public class ComponenteDAO {
@@ -21,8 +19,67 @@ public class ComponenteDAO {
             manager.getTransaction().commit();
         } catch(Exception ex){
             ex.getMessage();
-            System.out.println("Erro ao inserir componente: "+ex);
+            System.out.println("Erro ao inserir um componente: "+ex);
         }
+    }
+    
+    public List<Componente>consultarComponentes(){
+        
+        EntityManager manager = new JPAUtil().getEntityManager();
+        List<Componente> listaComponentes = new ArrayList<>();
+        TypedQuery<Componente> query = manager.createQuery("select comp from Componente comp join comp.atributo",Componente.class);
+        listaComponentes = query.getResultList();
+        return listaComponentes;
+        
+    }
+    
+    
+    //Método para deleter um Componente e suas dependências (TipoComponente)
+    
+    public void deletarComponente(Componente componente){
+        
+        EntityManager manager = new JPAUtil().getEntityManager();
+        manager.getTransaction().begin();
+        try{
+            componente = manager.find(Componente.class, componente.getId());
+            manager.remove(componente);
+            manager.getTransaction().commit();
+        } catch(Exception ex){
+            ex.getMessage();
+            System.out.println("Não foi possível deletar um Componente: "+ex);
+            manager.getTransaction().rollback();
+        } finally {
+            manager.close();
+        }
+    }
+    
+    public void alterarComponente(Componente componente){
+        
+        EntityManager manager = new JPAUtil().getEntityManager();
+        manager.getTransaction().begin();
+        try{
+            manager.merge(componente);
+            manager.getTransaction().commit();
+        } catch(Exception ex){
+            ex.getMessage();
+            System.out.println("Não foi possível atualizar o Componente: "+ex);
+            manager.getTransaction().rollback();
+        } finally {
+            manager.close();
+        }
+    }
+    
+    public Componente consultarPorIdComponente(Componente componente){
+        EntityManager manager = new JPAUtil().getEntityManager();
+        try{
+            componente = manager.find(Componente.class, componente.getId());
+        } catch(Exception ex){
+            ex.getMessage();
+            System.out.println("Não foi possível localizar o Componente com o  ID "+componente.getId()+ " : "+ex);
+        } finally {
+            manager.close();
+        }
+        return componente;
     }
      
 }
